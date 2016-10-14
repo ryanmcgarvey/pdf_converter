db5297495f93c89a135b82d87cb19b1f90780f50
Ryan Mcgarvey <ryan@themcgarveys.io>
2016-10-14 14:38:52 -0400

    Gets converter working

--- Gemfile
+++ Gemfile
@@ -1,11 +1,7 @@
 source 'https://rubygems.org'
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-# Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
@@ -23,6 +19,9 @@ gem 'puma', '~> 3.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'pry-byebug'
+  gem 'pry'
+
 end
 
 group :development do
@@ -33,4 +32,4 @@ group :development do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/controllers/pdf_controller.rb
@@ -0,0 +1,38 @@
+require 'securerandom'
+class PdfController < ApplicationController
+  def create
+    if params[:pdf_content].is_a? String
+      pdf_file_name = file_name + '.pdf'
+      file = File.new(pdf_file_name, 'w+')
+      file.write(params[:pdf_content])
+      execute(file.path)
+      File.delete(file)
+    else
+      execute(params[:pdf_content].tempfile.path)
+    end
+  end
+
+  def from_content
+    pdf_content = params[:pdf_content]
+    pdf_file_name = SecureRandom.hex + '.html'
+    execute(pdf_file_name)
+  end
+
+  private
+
+  def execute(pdf_file_name)
+    cmd = "pdf2htmlex --data-dir pdf2htmlex_config --printing=0 --process-outline=0 #{pdf_file_name} #{html_file_name}"
+    Rails.logger.error `#{cmd}`
+    html_content = File.read(html_file_name)
+    render html: html_content.html_safe
+    File.delete(html_file_name)
+  end
+
+  def html_file_name
+    @_html_file_name ||= file_name + '.html'
+  end
+
+  def file_name
+    SecureRandom.hex
+  end
+end
