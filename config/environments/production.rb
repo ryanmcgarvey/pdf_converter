db33b87efed1288942de68818480673da7f29f5a
Ryan Mcgarvey <ryan@themcgarveys.io>
2016-10-18 10:37:37 -0400

    production logs info only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,10 +34,6 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
-
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
@@ -73,7 +69,7 @@ Rails.application.configure do
   # logger           = ActiveSupport::Logger.new(STDOUT)
   # logger.formatter = config.log_formatter
   # config.logger = ActiveSupport::TaggedLogging.new(logger)
-  config.log_level = :debug
+  config.log_level = :info
 
   # Do not dump schema after migrations.
   # config.active_record.dump_schema_after_migration = false
